bba15ba575dde21c0279edab361c3a797bf363da
Ryan <rylarson17@gmail.com>
2022-12-04 23:36:48 -0700

    Inquirer prompts to show employees, roles and departments updated

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
 const inquirer = require("inquirer");
 const query = require("./query/query");
 
-// query.viewDepartments();
-// query.viewRoles();
-query.viewEmployees();
+const userAction = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "action",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department",
+          "Quit",
+        ],
+      },
+    ])
+    .then((response) => {
+      if (response.action === "View All Employees") {
+        query.viewEmployees();
+      }
+      if (response.action === "Add Employee") {
+        console.log("add employee");
+      }
+      if (response.action === "Update Employee Role") {
+        return;
+      }
+      if (response.action === "View All Roles") {
+        query.viewRoles();
+      }
+      if (response.action === "Add Role") {
+        return;
+      }
+      if (response.action === "View All Departments") {
+        query.viewDepartments();
+      }
+      if (response.action === "Add Department") {
+        return;
+      }
+      if (response.action === "Quit") {
+        query.quitDb();
+      }
+    });
+};
+
+userAction();
+exports.userAction = userAction;
--- query/query.js
+++ query/query.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql2");
 const cTable = require("console.table");
+const action = require("../index");
 require("dotenv").config();
 
 const db = mysql.createConnection({
@@ -10,34 +11,42 @@ const db = mysql.createConnection({
 });
 
 const viewDepartments = () => {
-  db.query(
-    "SELECT id,name AS department FROM departments",
-    function (err, result) {
-      console.table(result);
-    }
-  );
+  db.promise()
+    .query("SELECT departments.id,name AS department FROM departments;")
+    .then(([row, fields]) => {
+      console.table(row);
+    })
+    .then(() => action.userAction());
 };
 
 const viewRoles = () => {
-  db.query(
-    "SELECT role.id,title,name AS department, salary FROM departments JOIN role ON departments.id = role.department_id;",
-    function (err, result) {
-      console.table(result);
-      db.end();
-    }
-  );
+  db.promise()
+    .query(
+      "SELECT role.id,title,name AS department, salary FROM departments JOIN role ON departments.id = role.department_id;"
+    )
+    .then(([row, fields]) => {
+      console.table(row);
+    })
+    .then(() => action.userAction());
 };
 
 const viewEmployees = () => {
-  db.query(
-    "SELECT employee.id,employee.first_name,employee.last_name,title,name as department,salary,concat(manager.first_name,' ',manager.last_name) AS Manager FROM employee LEFT JOIN employee AS manager ON employee.manager_id=manager.id JOIN role ON employee.role_id =role.id JOIN departments ON role.department_id=departments.id;",
-    function (err, result) {
-      console.table(result);
-      db.end();
-    }
-  );
+  db.promise()
+    .query(
+      "SELECT employee.id,employee.first_name,employee.last_name,title,name as department,salary,concat(manager.first_name,' ',manager.last_name) AS Manager FROM employee LEFT JOIN employee AS manager ON employee.manager_id=manager.id JOIN role ON employee.role_id =role.id JOIN departments ON role.department_id=departments.id;"
+    )
+    .then(([row, fields]) => {
+      console.table(row);
+    })
+    .then(() => action.userAction());
+};
+
+const quitDb = () => {
+  db.promise().end();
+  return;
 };
 
 exports.viewDepartments = viewDepartments;
 exports.viewRoles = viewRoles;
 exports.viewEmployees = viewEmployees;
+exports.quitDb = quitDb;
